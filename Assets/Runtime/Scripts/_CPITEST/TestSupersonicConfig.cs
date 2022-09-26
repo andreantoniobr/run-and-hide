9d1a30de04c30f13893fe46a5fd19843548257ca
André Antônio <andreantoniobr@hotmail.com>
2022-09-26 12:54:51 -0300

    Add Cpi test Files

---
+++ Assets/Runtime/Scripts/_CPITEST/TestSupersonicConfig.cs
@@ -0,0 +1,12 @@
+using GameAnalyticsSDK;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSupersonicConfig : MonoBehaviour
+{
+    private void Start()
+    {
+        GameAnalytics.Initialize();
+    }
+}
